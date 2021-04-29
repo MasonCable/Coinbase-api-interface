2f9a360fa9921a1605bc3914e553d2e7b462f097
Mason C <mason.cable@protonmail.com>
2021-04-29 02:00:26 -0700

    Added hte started function for playing limit buys

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import time, requests
-from urls import *
 from CoinAccount import Account
+import argparse
 from auth_client import CBAuth
 from MarketData import MarketData
 from auth_credentials import PASSPHRASE, KEY, SECRET
+# Store user arguments as globals
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--buy", help="Buy $ASSET")
+parser.add_argument("-p", "--price", help="What Price Would you like to buy/sell this asset at?")
+parser.add_argument("-s", "--sell", help="Sell $ASSET")
+parser.add_argument("-ep", "--executionprice", help="What price would you like this order to be executed at?")
+args = parser.parse_args()
+inputAsset = args.buy if args.buy else args.sell
+inputBuyOrSell = 'buy' if args.buy else 'sell'
+buyOrSellPrice = args.price
+executionPrice = args.executionprice
 
-auth = CBAuth(KEY, SECRET, PASSPHRASE)
-accountData = Account()
-marketData = MarketData()
 
-for i in accountData.accounts():
-    if float(i['available']) != 0.00000000:
-        print(i)
-        
+def limit_buy(asset, price, executionPrice):
+    market = MarketData()
+    account = Account()
+    response = market.product_from_id(asset)
+    confirmTransaction = input('You would like to {0} ${1} worth of {2} when {2} reaches a price of ${3}\n (yes) Or (no)\n'.format(inputBuyOrSell, buyOrSellPrice ,inputAsset, executionPrice))
+    if confirmTransaction:
+        account.orders()
+    else:
+        return "Order Canceled, click up arrow to try again"
+
+
+print(limit_buy(inputAsset, buyOrSellPrice, executionPrice))
     
 
-run = False
-while run:
-    for i in accountData.accounts():
-        if i['available'] != '0':
-            print(i)
+
+
     
